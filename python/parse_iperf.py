c71d2b2a33f6920a6a5e2d7fc2269509fff2e98a
nitin-kuncham <nitin.kuncham@gmail.com>
2018-11-21 19:57:51 -0500

    created topology creation code

--- python/parse_iperf.py
+++ python/parse_iperf.py
@@ -1,1 +1,7 @@
-skjks
+#!/usr/bin/env python
+#Code to parse iperf output
+
+def parse_iperf_output(input_string):
+	
+
+
--- python/ssh_to_mininet.py
+++ python/ssh_to_mininet.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-#Code to sssh into mininet VM and execute bash script
+#Code to ssh into mininet VM and execute bash script
+
+#USAGE - sudo python ssh_to_mininet.py <type of service tag> <topology type>
 
 import sys
 import paramiko
@@ -10,11 +12,12 @@ argument_1=sys.argv[1]
 argument_2 = sys.argv[2]
 
 
-def to_mininet(ip_address):
+def to_mininet(ip_address,argument_1,argument_2):
+	
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(ip_address,username='mininet', password='mininet', port=22)
-
+	#command1="sudo python topology_creation.py argument_1 argument_2"
 	command1="ip addr"
 	stdin, stdout, stderr = ssh.exec_command(command1)
 	stdout.channel.recv_exit_status()
@@ -24,10 +27,10 @@ def to_mininet(ip_address):
 	command_1_output=command_1_output.lower()
 	print command_1_output
 	print ("\n\n\n")
+	
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	ssh.connect(ip_address,username='mininet', password='mininet', port=22)
-
 	command2="ip route"
 	stdin, stdout, stderr = ssh.exec_command(command2)
 	stdout.channel.recv_exit_status()
@@ -46,4 +49,4 @@ def to_mininet(ip_address):
 #THE NEXT LINE NEEDS TO BE UPDATED WITH THE IP ADDRESS OF MININET
 mininet_ip = "10.153.27.182"
 
-to_mininet(mininet_ip)
+to_mininet(mininet_ip,argument_1,argument_2)
