62c8b8691a555bddc4cf38999a2086d670cb10ca
None <None>
2018-11-23 11:33:18 -0800

    parse ip addr done

--- python/parse_ip_addr.py
+++ python/parse_ip_addr.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 #Code to parse ip addr output
+import re
 
 def parse_ip_addr_output(input_string):
 	ip_address=""
-
 	#Type your code here
-	
+
+	ip = r'(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})\.(?:[\d]{1,3})'
+	found_ip = re.findall( ip,text )	
+	ip_address = found_ip[1]
+
 	return (ip_address)
 
 
