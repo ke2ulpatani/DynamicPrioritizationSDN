86d093b0b390c1d46f4638d166c8f82838aeae17
None <None>
2018-12-03 14:08:40 -0800

    demoed codes

--- python/pox_configure.py
+++ python/pox_configure.py
@@ -13,7 +13,7 @@ with open("pox/pox/forwarding/mac_to_be_blocked.txt","w") as file:
         file.write(mac_to_be_blocked)
 with open("pox/pox/forwarding/qos.txt","w") as file:
         file.write(tos)
-temp1="ovs-vsctl -- set Port s1-eth3 qos=@newqos -- --id=@newqos create QoS type=linux-htb other-config:max-rate=1000000000 queues=0=@q0,1=@q1 -- --id=@q0 create Queue other-config:min-rate=1000000000 other-config:max-rate=1000000000 -- --id=@q1 create Queue other-config:min-rate="+str(bandwidth)+"000000 other-config:max-rate="+str(bandwidth)+"000000"
+temp1="ovs-vsctl -- set Port s1-eth4 qos=@newqos -- --id=@newqos create QoS type=linux-htb other-config:max-rate=1000000000 queues=0=@q0,1=@q1 -- --id=@q0 create Queue other-config:min-rate=1000000000 other-config:max-rate=1000000000 -- --id=@q1 create Queue other-config:min-rate="+str(bandwidth)+"000000 other-config:max-rate="+str(bandwidth)+"000000"
 temp2=temp1.split()
 subprocess.call(temp2)
 
