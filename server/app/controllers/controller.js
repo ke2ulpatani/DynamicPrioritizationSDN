1a64cec2cd8b49f10afdcbb2265835911cbe83aa
ke2ulpatani <ke2ul.patani@gmail.com>
2018-12-03 14:19:14 -0500

    little fixes in UI

--- server/app/controllers/controller.js
+++ server/app/controllers/controller.js
@@ -1,109 +1,22 @@
 var exec = require('child_process').exec;
 
 exports.createTopology = function(request, response) {
-    // request.body = JSON.parse(request.body);
-
-    // var yale = JSON.parse(request.body);
-
-	var shellCommand = "python ../python/ssh_to_mininet.py "+request.body.service_name+" "+ request.body.topology+" &";
-
+	var shellCommand = "python ../python/ssh_to_mininet.py "+request.body.topology+" "+request.body.service_name+" "+ request.body.mac_address+" "+request.body.bandwidth+" &";
+    // console.log("this is the body:\t"+response);
+    // console.log(shellCommand);
     
+    exec(shellCommand, function(error, stdout, stderr) {
 
-    console.log("this is the body:\t"+response);
-    console.log(shellCommand);
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
     
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "*");
     response.header("Access-Control-Request-Method", "*");
-    
-
-    // var output = {"service_name":}
     response.end();
 
-	
+
 };
-
-
-
-
-
-
-
-    // var shellCommand = "python ../python/ssh_to_mininet.py "+yale.service_name+" "+ yale.topology+" &";
-    
-    // exec(shellCommand, function(error, stdout, stderr) {
-
- //    console.log('stdout: ' + stdout);
- //    console.log('stderr: ' + stderr);
- //    if (error !== null) {
- //        console.log('exec error: ' + error);
- //    }
-
-// var customers = {
-// 				customer1: {
-// 					firstname: "Jack",
-// 					lastname: "Davis",
-// 					age: 25,
-// 					id: 1
-// 				},
-// 				customer2: {
-// 					firstname: "Mary",
-// 					lastname: "Taylor",
-// 					age: 37,
-// 					id: 2
-// 				},
-// 				customer3: {
-// 					firstname: "Peter",
-// 					lastname: "Thomas",
-// 					age: 17,
-// 					id: 3
-// 				},
-// 				customer4: {
-// 					firstname: "Peter",
-// 					lastname: "Thomas",
-// 					age: 17,
-// 					id: 4
-// 				}
-// 			}
- 
-// exports.create = function(req, res) {
-// 	var newCustomer = req.body;
-//     customers["customer" + newCustomer.id] = newCustomer;
-// 	console.log("--->After Post, customers:\n" + JSON.stringify(customers, null, 4));
-//     res.end("Post Successfully: \n" + JSON.stringify(newCustomer, null, 4));
-// };
- 
-// exports.findAll = function(req, res) {
-//     console.log("--->Find All: \n" + JSON.stringify(customers, null, 4));
-//     res.end("All Customers: \n" + JSON.stringify(customers, null, 4));  
-// };
- 
-// exports.findOne = function(req, res) {
-//     var customer = customers["customer" + req.params.id];
-//     console.log("--->Find customer: \n" + JSON.stringify(customer, null, 4));
-//     res.end( "Find a Customer:\n" + JSON.stringify(customer, null, 4));
-// };
- 
-// exports.update = function(req, res) {
-// 	var id = parseInt(req.params.id);
-// 	var updatedCustomer = req.body; 
-// 	if(customers["customer" + id] != null){
-// 		// update data
-// 		customers["customer" + id] = updatedCustomer;
- 
-// 		console.log("--->Update Successfully, customers: \n" + JSON.stringify(customers, null, 4))
-		
-// 		// return
-// 		res.end("Update Successfully! \n" + JSON.stringify(updatedCustomer, null, 4));
-// 	}else{
-// 		res.end("Don't Exist Customer:\n:" + JSON.stringify(updatedCustomer, null, 4));
-// 	}
-// };
- 
-// exports.delete = function(req, res) {
-// 	var deleteCustomer = customers["customer" + req.params.id];
-//     delete customers["customer" + req.params.id];
-//     console.log("--->After deletion, customer list:\n" + JSON.stringify(customers, null, 4) );
-//     res.end( "Deleted customer: \n" + JSON.stringify(deleteCustomer, null, 4));
-// };
-
