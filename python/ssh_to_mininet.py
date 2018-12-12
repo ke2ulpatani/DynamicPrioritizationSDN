15904a66c4d5d33ac40c739be32a8604f0d0eb89
nitin-kuncham <nitin.kuncham@gmail.com>
2018-12-12 12:12:18 -0800

    handle 2 demos

--- python/ssh_to_mininet.py
+++ python/ssh_to_mininet.py
@@ -13,7 +13,7 @@ tos = sys.argv[2]
 mac_to_be_blocked=sys.argv[3]
 bandwidth=sys.argv[4]
 
-def to_mininet(ip_address,topology,tos,mac_to_be_blocked,bandwidth):
+def to_mininet1(ip_address,topology,tos,mac_to_be_blocked,bandwidth):
 	
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -26,21 +26,51 @@ def to_mininet(ip_address,topology,tos,mac_to_be_blocked,bandwidth):
 	command_1_output = stdout.read().replace('\n',' ')
 	command_1_output=command_1_output.replace('\r','')
 	command_1_output=command_1_output.lower()
-	print command_1_output
-	print ("\n\n\n")
-	time.sleep(8)
-	ssh = paramiko.SSHClient()
-	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(ip_address,username='mininet', password='mininet', port=22)
-	#command2="cat iperf_results"
-	command2="ip route"
-	stdin, stdout, stderr = ssh.exec_command(command2)
-	stdout.channel.recv_exit_status()
-	ssh.close()
-	command_2_output = stdout.read().replace('\n',' ')
-	command_2_output=command_2_output.replace('\r','')
-	command_2_output=command_2_output.lower()
-	print command_2_output
+	#print command_1_output
+	#print ("\n\n\n")
+	#time.sleep(8)
+	#ssh = paramiko.SSHClient()
+	#ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	#ssh.connect(ip_address,username='mininet', password='mininet', port=22)
+	##command2="cat iperf_results"
+	#command2="ip route"
+	#stdin, stdout, stderr = ssh.exec_command(command2)
+	#stdout.channel.recv_exit_status()
+	#ssh.close()
+	#command_2_output = stdout.read().replace('\n',' ')
+	#command_2_output=command_2_output.replace('\r','')
+	#command_2_output=command_2_output.lower()
+	#print command_2_output
+
+
+def to_mininet2(ip_address,topology,tos,mac_to_be_blocked,bandwidth):
+
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip_address,username='mininet', password='mininet', port=22)
+        #command1="python /home/mininet/pox_configure.py "+str(tos)+" "+str(mac_to_be_blocked)+" "+str(bandwidth)+" &"
+        command1="ip addr"
+        stdin, stdout, stderr = ssh.exec_command(command1)
+        stdout.channel.recv_exit_status()
+        ssh.close()
+        command_1_output = stdout.read().replace('\n',' ')
+        command_1_output=command_1_output.replace('\r','')
+        command_1_output=command_1_output.lower()
+        #print command_1_output
+        #print ("\n\n\n")
+        #time.sleep(8)
+        #ssh = paramiko.SSHClient()
+        #ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        #ssh.connect(ip_address,username='mininet', password='mininet', port=22)
+        ##command2="cat iperf_results"
+        #command2="ip route"
+        #stdin, stdout, stderr = ssh.exec_command(command2)
+        #stdout.channel.recv_exit_status()
+        #ssh.close()
+        #command_2_output = stdout.read().replace('\n',' ')
+        #command_2_output=command_2_output.replace('\r','')
+        #command_2_output=command_2_output.lower()
+        #print command_2_output
 
 #----------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------------------------
@@ -51,4 +81,7 @@ def to_mininet(ip_address,topology,tos,mac_to_be_blocked,bandwidth):
 #THE NEXT LINE NEEDS TO BE UPDATED WITH THE IP ADDRESS OF MININET
 mininet_ip = "192.168.1.19"
 
-to_mininet(mininet_ip,topology,tos,mac_to_be_blocked,bandwidth)
+if topology=="na":
+	to_mininet1(mininet_ip,topology,tos,mac_to_be_blocked,bandwidth)
+elif topology=="linear" or topology=="tree"
+	to_mininet2(mininet_ip,topology,tos,mac_to_be_blocked,bandwidth)
